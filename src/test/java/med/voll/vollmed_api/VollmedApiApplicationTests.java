0439daff01a1225962fc10a8c4c655266a3cd5bb
zeotoni <zeotoni666@gmail.com>
2024-07-26 16:17:18 -0300

    test: adds contextLoads test

--- src/test/java/med/voll/vollmed_api/VollmedApiApplicationTests.java
+++ src/test/java/med/voll/vollmed_api/VollmedApiApplicationTests.java
@@ -1,12 +1,20 @@
 package med.voll.vollmed_api;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
-public class VollmedApiApplicationTests {
+class VollmedApiApplicationTests {
+
+    @Autowired
+    private ApplicationContext applicationContext;
 
     @Test
     void contextLoads() {
+        assertThat(applicationContext).isNotNull();
     }
 }
